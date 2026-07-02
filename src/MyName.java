d0c5463251548f8cbd61edb96a6536618c6ee375
master <vfxhmaster@gmail.com>
2026-07-02 13:17:04 +0300

    initial commit

--- src/MyName.java
+++ src/MyName.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+/**Документирующий коментарий
+ * @author Max Komisaorov
+ * @version 1.0.2026
+ *
+ */
+// Создание класа
+public class MyName {
+    //Точка входа в программу
+    public static void main(String[] args) {
+        // Однострочный коментарий
+        /*
+        Много строчный коментарий
+         */
 
-public class FirstLesson {
+        /*
+        System.out.println -> Выводит текст на екран в новой строке
+         */
+
+        System.out.println("My name is Max");
+    }
 }
